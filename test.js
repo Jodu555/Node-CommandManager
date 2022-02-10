e54f1667fa96614c6a3ddd6f37a63083ed7a50db
Jodu555 <Jodu505@gmail.com>
2022-02-10 11:47:43 +0100

    Played a bit with the actual clearLine write etc and started to engineer a lib

--- test.js
+++ test.js
@@ -1,14 +1,33 @@
-const { CommandManager, Command } = require('./src/index')
+// const { CommandManager, Command } = require('./src/index')
 
-const commandManager = CommandManager.createCommandManager(process.stdin, process.stdout);
+// const commandManager = CommandManager.createCommandManager(process.stdin, process.stdout);
 
-require('./test2');
+// require('./test2');
 
-commandManager.registerCommand(new Command('info', 'info', 'Displays the info!', (command, [...args], scope) => {
-    //Do Some code here
-    console.log(args, scope);
-    return ['INFO', 'INFO', 'INFO']
-}));
+// commandManager.registerCommand(new Command('info', 'info', 'Displays the info!', (command, [...args], scope) => {
+//     //Do Some code here
+//     console.log(args, scope);
+//     return ['INFO', 'INFO', 'INFO']
+// }));
 
-commandManager.callCommand('info hallo123 123');
+// commandManager.callCommand('info hallo123 123');
 
+const sleep = time => new Promise((resolve, reject) => setTimeout(() => resolve(), time));
+
+
+async function run(params) {
+    for (let i = 0; i < 100; i += 10) {
+        await sleep(500);
+        printProgress(i)
+    }
+    console.log('Hallo2');
+}
+
+function printProgress(progress) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(progress + '%');
+}
+
+run();
+console.log('Hallo');
