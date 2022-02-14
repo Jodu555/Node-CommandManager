eb1efd4f4ce64006d2819f0eb41c9ac5929ab1f3
Jodu555 <Jodu505@gmail.com>
2022-02-14 20:31:53 +0100

    Fixed

--- test.js
+++ test.js
@@ -16,26 +16,23 @@ const sleep = time => new Promise((resolve, reject) => setTimeout(() => resolve(
 
 const writer = commandManager.getWriter();
 async function run(params) {
-    // for (let i = 0; i < 101; i += 1) {
-    //     await sleep(50);
-    //     writer.displayProgress(i, 100, 1, 2);
-    // }
-    // writer.end();
-    // console.log('Hallo2');
-
-    writer.deepSameLineClear('Hallo 123');
-    await sleep(1000);
-    writer.deepSameLineClear('Hallo 456');
-    await sleep(1000);
-    writer.deepSameLineClear(`Hallo 123.2\nTEST 123.2`);
-    await sleep(1000);
-    writer.deepSameLineClear(`Hallo 456.2\nTEST 456.2`, 2);
+    for (let i = 0; i < 101; i += 1) {
+        await sleep(50);
+        writer.displayProgress(i, 100, 1, 2);
+    }
     writer.end();
+    console.log('Hallo2');
+
+    // writer.deepSameLineClear('Hallo 123');
+    // await sleep(1000);
+    // writer.deepSameLineClear('Hallo 456');
+    // await sleep(1000);
+    // writer.deepSameLineClear(`Hallo 123.2\nTEST 123.2`);
+    // await sleep(1000);
+    // writer.deepSameLineClear(`Hallo 456.2\nTEST 456.2`, 2);
+    // writer.end();
 }
 
 
 run();
 // console.log('Hallo');
-
-
-
