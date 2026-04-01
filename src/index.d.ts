eef9bda7f124a1bf1f7a2ace6dfc4d05edd8ba15
Jodu555 <Jodu505@gmail.com>
2026-04-01 16:08:02 +0200

    extracted CommandCallback as the command callback

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,26 +1,20 @@
 declare module '@jodu555/commandmanager' {
     interface IOverCommandManager {
-        createCommandManager: (streamIn, streamOut) => CommandManagerClass;
-        getCommandManager: () => CommandManagerClass;
+        createCommandManager: (streamIn, streamOut) => CommandManager;
+        getCommandManager: () => CommandManager;
     }
 
-    type CommandCallback = (command: string, args: string[], scope: string) => void;
+    type CommandCallback = (command: string, args: string[], scope: string) => void
 
     class Command {
         ID: number;
         command: string | string[];
         usage: string;
         description: string;
-        callback: CommandCallback;
         constructor(command: string | string[], usage: string, description: string, callback: CommandCallback);
     }
 
-    class CommandManagerClass {
-        streamIn: any;
-        streamOut: any;
-        commands: Map<string, Command>;
-        cli: any;
-        writer: Writer;
+    class CommandManager {
         refresh: () => void;
         callCommand: (line: string, scope: string) => void;
         getAllCommandsWithoutAliases: () => Command[];
@@ -30,17 +24,23 @@ declare module '@jodu555/commandmanager' {
         unregisterCommand: (command: string) => void;
         deleteCommand: (ID: number) => void;
         getWriter: () => Writer;
+        streamIn: any;
+        streamOut: any;
+        commands: Map<string, Command>;
+        cli: any;
+        writer: Writer;
+        init: () => void;
     }
 
     interface SymbolsObject {
-        leftBorder: string;
-        rightBorder: string;
-        loaded: string;
-        notLoaded: string;
+        leftBorder: string = '[';
+        rightBorder: string = ']';
+        loaded: string = '#';
+        notLoaded: string = '-';
     }
 
     class Writer {
-        commandManager: CommandManagerClass;
+        commandManager: CommandManager;
         stdout: any;
         displayProgress: (progress: number, max: number, type: number, steps?: number, smybols?: SymbolsObject, appender?: string) => void;
         deepSameLineClear: (data: string, l: number) => void;
